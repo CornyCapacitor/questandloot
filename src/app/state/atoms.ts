5bc1720f2bab09c41a25d6bd1f2d702785cc7b82
Mateusz Minder <mateusz.minder99@gmail.com>
2024-10-25 22:00:40 +0200

    swapping playerAtom to atomWithStorage

--- src/app/state/atoms.ts
+++ src/app/state/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 import { Monster, Player } from "../types";
 
-// export const playerAtom = atom<Player | null>(null)
-export const playerAtom = atom<Player | null>(null)
+export const playerAtom = atomWithStorage<Player | null>('playerAtom', null)
 export const enemyAtom = atom<Monster | null>(null)
