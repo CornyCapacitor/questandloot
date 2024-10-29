9247bf05f25e4e4063eb550feedbf44879f1fa52
Mateusz Minder <mateusz.minder99@gmail.com>
2024-10-29 22:28:45 +0100

    fix: quick fix for proper compilation

--- src/components/layout/ItemFrame.tsx
+++ src/components/layout/ItemFrame.tsx
@@ -1,4 +1,4 @@
-import { Items } from '@/app/types'
+import { Items, Potion } from '@/app/types'
 import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip'
@@ -35,14 +35,16 @@ const ItemFrame = ({ itemData, isClickable, isDisabled, isEquipped }: { itemData
             </div>
           </TooltipTrigger>
           <TooltipContent>
-            <div>{itemData.name}</div>
+            <div className="flex flex-col">
+              <h1>{itemData.name}</h1>
+            </div>
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
 
       {isActiveTooltipVisible && (
         <div className="absolute flex flex-col gap-2 a-10 bg-black border border-slate-700 rounded-md text-sm p-2 w-[128px] tooltip-button-animation z-10">
-          <button className="w-full bg-slate-800 py-1 px-2 rounded-md">Equip</button>
+          <button className="w-full bg-slate-800 py-1 px-2 rounded-md">{isEquipped ? 'Unequip' : "Equip"}</button>
           <button className="w-full bg-slate-800 py-1 px-2 rounded-md">Sell</button>
         </div>
       )}
@@ -51,3 +53,14 @@ const ItemFrame = ({ itemData, isClickable, isDisabled, isEquipped }: { itemData
 }
 
 export default ItemFrame
+
+// Exporting just for compiler
+export const PotionFrame = (item: Potion) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className={`${item.quality === 'uncommon' ? 'text-green-500' : item.quality === 'rare' ? 'text-blue-500' : item.quality === 'epic' ? 'text-purple-600' : 'text-white'}`}>{item.name}</h1>
+      <h2 className="text-gray-300 text-sm text-wrap">{item.description}</h2>
+      <h3 className="text-wrap">Increases {item.enchancing.attribute} by {item.enchancing.value}% for 24h</h3>
+    </div>
+  )
+}
