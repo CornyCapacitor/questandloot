7f2b58dd1af7fad7dae157da8013a352c51c818a
Mateusz Minder <mateusz.minder99@gmail.com>
2024-11-09 23:20:22 +0100

    Connection setup with the game server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,59 @@ import { useAtom } from "jotai";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { dummyPlayer } from "./dummies";
+import { io, Socket } from 'socket.io-client';
 import { playerAtom } from "./state/atoms";
 
 export default function Home() {
-  const [login, setLogin] = useState('')
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [, setPlayer] = useAtom(playerAtom)
+  const [socket, setSocket] = useState<Socket | null>(null)
   const router = useRouter()
 
-  const dummyLogin = 'login'
-  const dummyPassword = 'password'
+  const handleLogin = async (username: string, password: string) => {
+    try {
+      // Wysłanie POST do serwera REST
+      const response = await fetch('http://localhost:3333/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-  const handleLogin = (login: string, password: string) => {
-    if (login === dummyLogin && password === dummyPassword) {
-      alert('Authenticated. Logging in...')
-      setPlayer(dummyPlayer)
-      router.push('/game')
-      return
-    } else {
-      alert('Invalid credentials.')
-      return
+      // Sprawdzanie odpowiedzi serwera
+      if (!response.ok) {
+        throw new Error('Invalid credentials');
+      }
+
+      // Jeśli odpowiedź jest OK, pobieramy token
+      const data = await response.json();
+      const token = data.token;
+
+      console.log(token)
+
+      // Zwracamy token
+      return token;
+    } catch (error) {
+      console.error('Error during login:', error);
+      return null;
     }
+  }
+
+  const handleConnect = async (username: string, password: string) => {
+    console.log()
+    const token = await handleLogin(username, password)
+
+    const ws = io('http://localhost:3334', {
+      query: { token }
+    })
+
+    setSocket(ws)
+
+    ws.on('connect', () => {
+      console.log('Connected to websocket server:', ws.id)
+    })
   }
 
   return (
@@ -35,11 +66,11 @@ export default function Home() {
       <section className="h-full p-10 min-w-[350px] max-w-[600px] flex flex-col gap-2 items-center justify-center border-r bg-slate-800 border-slate-700">
         <h1>Welcome to Quest & Loot!</h1>
         <span>In order to access the game you need to be logged in. (login and password are credentials)</span>
-        <label htmlFor="nickname" className="self-start text-sm text-slate-200">Login</label>
-        <Input id="Login" autoFocus placeholder="Login" className="w-full p-3 bg-slate-700 text-slate-100 border border-slate-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" value={login} onChange={(e) => setLogin(e.target.value)} />
-        <label htmlFor="nickname" className="self-start text-sm text-slate-200">Password</label>
+        <label htmlFor="username" className="self-start text-sm text-slate-200">Username</label>
+        <Input id="username" autoFocus placeholder="Username" className="w-full p-3 bg-slate-700 text-slate-100 border border-slate-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <label htmlFor="password" className="self-start text-sm text-slate-200">Password</label>
         <Input id="password" type="password" placeholder="Password" className="w-full p-3 bg-slate-700 text-slate-100 border border-slate-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <Button className="bg-blue-500 hover:bg-blue-600 text-white" onClick={() => handleLogin(login, password)}>Login</Button>
+        <Button className="bg-blue-500 hover:bg-blue-600 text-white" onClick={() => handleConnect(username, password)}>Login</Button>
         <span>No account? Create one right now!</span>
         <Button className="bg-blue-500 hover:bg-blue-600 text-white">Signup</Button>
       </section>
