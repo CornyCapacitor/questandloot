f0078a76eccab89cbb312df0e28c0d44b7197be3
Mateusz Minder <mateusz.minder99@gmail.com>
2024-11-20 21:26:13 +0100

    new sidebar icons

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
     <aside className="w-full max-w-[300px] h-full flex flex-col gap-4 px-2 py-4 items-center justify-between border-r bg-slate-800 border-slate-700">
       <div className=" flex flex-col gap-4 w-[90%]">
         <h1 className="flex gap-1 justify-center">Gold: {player.gold} <Image width={20} height={20} src="/coin.svg" alt="Gold coin" /></h1>
-        <GameButton path="/game/character" className="flex items-center justify-between px-8 py-2">Character <Image src="/assets/sidebar/character.svg" height={40} width={40} alt="character icon" /></GameButton>
+        <GameButton path="/game/character" className="flex items-center justify-between px-8 py-2">Character <Image src={`/assets/portraits/${player.image}`} height={40} width={40} alt="character icon" /></GameButton>
         <GameButton path="/game/journey" className="flex items-center justify-between px-8 py-2">Journey <Image src="/assets/sidebar/journey.svg" height={40} width={40} alt="journey icon" /></GameButton>
         <GameButton path="/game/shop" className="flex items-center justify-between px-8 py-2">Merchants <Image src="/assets/sidebar/merchants.svg" height={40} width={40} alt="merchants icon" /></GameButton>
         <GameButton path="/game/crafting" className="flex items-center justify-between px-8 py-2">Crafting <Image src="/assets/sidebar/crafting.svg" height={40} width={40} alt="character icon" /></GameButton>
